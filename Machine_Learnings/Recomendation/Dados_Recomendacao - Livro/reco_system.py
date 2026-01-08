53286e81a1b779e23bb0d1a18eb951c95dfe4525
jvecodev <jvitor.oliveira1803@gmail.com>
2026-01-07 22:34:02 -0300

    feat: Adiciona arquivos de dados e implementa sistema de recomendação

---
+++ Machine_Learnings/Recomendation/Dados_Recomendacao - Livro/reco_system.py
@@ -0,0 +1,32 @@
+import pandas as pd #trabalhar com dados
+import numpy as np #operações matemáticas/matrizes
+import matplotlib.pyplot as plt #visualização de dados
+import seaborn as sns #visualização de dados
+import plotly.express as px #visualização interativa
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# Ajustes do pandas
+
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 50)
+
+# Ajustes no matplotlib
+# Tamanho do grafico
+plt.rcParams['figure.figsize'] = (15, 6)
+plt.style.use('dark_background') # estilo do grafico
+
+
+# Ler os dados 
+
+dados_livres = pd.read_csv('Books.csv')
+dados_avaliacao = pd.read_csv('Ratings.csv')
+dados_usuarios = pd.read_csv('Users.csv')
+
+# Dimensões dos dados
+
+print("Dimensões dos dados:")
+print(f"Avaliações: {dados_avaliacao.shape}")
+print(f"Livros: {dados_livres.shape}")
+print(f"Usuários: {dados_usuarios.shape}")
--- Machine_Learnings/shapiro.py
+++ Machine_Learnings/shapiro.py
@@ -5,6 +5,19 @@ import scipy.stats as stats
 dados = [12, 15, 16, 17, 18, 20, 21, 25, 26, 27, 30, 35]
 
 
+"""
+Teste de shapiro-Wilk se baseia na comparação 
+entre a distribuição dos dados e uma 
+distribuição normal teórica.
+
+Por exemplo:
+
+H0: Os dados seguem uma distribuição normal.
+H1: Os dados não seguem uma distribuição normal.
+Se o valor p for maior que 0.05, não rejeitamos H0,
+
+"""
+
 # stat é o valor do teste, quanto mais proximo de 1 melhor
 # pval é o valor p, se for maior que 0.05 aceita a hipótese nula
 # Este é um teste de normalidade para verificar se os dados seguem uma distribuição normal
